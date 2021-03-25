3f344a55bad45fd9cc86225097775ca2785431f9
Ramin Mammadzada <remki-design@hotmail.com>
2021-03-25 08:45:13 +0300

    Implement CREATE_BOOK and REMOVE_BOOK actions inside books reducer.

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,5 +1,15 @@
+import { CREATE_BOOK, REMOVE_BOOK } from '../constants/action-types';
 import { books as booksArrays } from '../static-data';
 
-export default function books(store = booksArrays) {
+export default function books(store = booksArrays, action) {
+  if (action.type === CREATE_BOOK) {
+    return [
+      ...store, action.payload,
+    ];
+  }
+
+  if (action.type === REMOVE_BOOK) {
+    return store.filter(book => book.id.toString() !== action.payload);
+  }
   return store;
 }
